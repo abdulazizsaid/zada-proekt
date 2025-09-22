f118d99d438bca86e0c232c07b757aac3123aea6
abdulazizsaidovcode <abdulazizsaidovcode@gmail.com>
2025-09-22 14:23:49 +0500

    uzur uzur adashib ketdim

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -27,7 +27,7 @@ let interval;
 onMounted(() => {
     interval = setInterval(() => {
         currentIndex.value = (currentIndex.value + 1) % components.length;
-    }, 3000); // har 7 sekundda almashadi
+    }, 30000); // har 7 sekundda almashadi
 });
 
 onBeforeUnmount(() => {
